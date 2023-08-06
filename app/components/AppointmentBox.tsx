659d601d92eace4a0b67d292bd48ec545c930fec
Tan Yan-Hao Joshua <joshuatanyanhao@gmail.com>
2023-08-06 18:13:04 +0800

    Add expand button

--- app/components/AppointmentBox.tsx
+++ app/components/AppointmentBox.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { BiChevronDownCircle } from 'react-icons/bi';
+
 import DateInput from "./inputs/DateInput";
 import { SafePet } from "../types";
 import { useState } from "react";
@@ -63,7 +65,7 @@ const AppointmentBox: React.FC<AppointmentBoxProps> = ({ pet }) => {
                 <div className="flex">
                     <div className="relative w-1/2 border-r p-3 pl-5">
                         <p className="text-lg font-semibold">Date</p>
-                        <button className="underline underline-offset-2 font-light" onClick={toggleShowDatePicker}>{appointmentDate.getDate()}/{appointmentDate.getMonth()}/{appointmentDate.getFullYear()}</button>
+                        <button className="underline underline-offset-2 font-light hover:text-sky-500 transition" onClick={toggleShowDatePicker}>{appointmentDate.getDate()}/{appointmentDate.getMonth()}/{appointmentDate.getFullYear()}</button>
                         {showDatePicker && (
                             <div className="absolute z-10 top-13 right-0 bg-white p-3 shadow-2xl rounded-xl">
                                 <DateInput date={appointmentDate} onChange={(date: Date) => setAppointmentDate(date)}/>
@@ -74,7 +76,7 @@ const AppointmentBox: React.FC<AppointmentBoxProps> = ({ pet }) => {
                     
                     <div className="relative w-1/2 p-3 pl-5">
                         <p className="text-lg font-semibold">Time</p>
-                        <button className="underline underline-offset-2 font-light" onClick={toggleShowTimePicker}>{appointmentTime.getHours()} : {appointmentTime.getMinutes()} hrs</button>
+                        <button className="underline underline-offset-2 font-light hover:text-sky-500 transition" onClick={toggleShowTimePicker}>{appointmentTime.getHours()} : {appointmentTime.getMinutes()} hrs</button>
                         {showTimePicker && (
                             <div className="absolute z-10 top-13 right-0 bg-white p-3 shadow-2xl rounded-xl">
                                 <p>TODO: Implement Time Picker</p>
@@ -84,12 +86,12 @@ const AppointmentBox: React.FC<AppointmentBoxProps> = ({ pet }) => {
                     </div>
                 </div>
                 
-                <hr className='border-r border-neutral-200'/>
+                <hr className='border-r'/>
                 
-                <div className="flex">
+                <div className="flex justify-between">
                     <div className="relative w-1/2 p-3 pl-5">
                         <p className="text-lg font-semibold">Number of Visitors</p>
-                        <button className="underline underline-offset-2 font-light" onClick={toggleShowNumberPicker}>{numberOfVisitors} persons</button>
+                        <button className="underline underline-offset-2 font-light hover:text-sky-500 transition" onClick={toggleShowNumberPicker}>{numberOfVisitors} persons</button>
                         {showNumberPicker && (
                             <div className="absolute z-10 top-13 left-3 bg-white p-3 shadow-2xl rounded-xl">
                                 <p>TODO: Implement Number Picker</p>
@@ -97,6 +99,9 @@ const AppointmentBox: React.FC<AppointmentBoxProps> = ({ pet }) => {
                             </div>
                         )}
                     </div>
+                    <button className='p-3 pr-5 self-center' onClick={toggleShowNumberPicker}>
+                        <BiChevronDownCircle size={24} className='fill-neutral-700 hover:fill-sky-500 transition'/>
+                    </button>
                 </div>
             </div>
             
@@ -105,9 +110,9 @@ const AppointmentBox: React.FC<AppointmentBoxProps> = ({ pet }) => {
             </div>
 
             <div className="w-5/6 flex gap-1 place-items-center">
-                <hr className='w-1/2 border-neutral-200'/>
+                <hr className='w-1/2'/>
                 <p>OR</p>
-                <hr className='w-1/2 border-neutral-200'/>
+                <hr className='w-1/2'/>
             </div>
             
             <div className="w-5/6 mb-8">
